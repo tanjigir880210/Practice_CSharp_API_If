dd0b7e7e270d5e12fd88d76ffd2c3575d8630b77
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2019-10-17 12:19:24 +0800

    作業半成品

---
+++ UnityProject/Assets/sprits/chickencontrol.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chickencontrol : MonoBehaviour
+
+{
+
+    Animator Chicken_Anime;
+    bool 跑步開關;
+
+    void Start()
+
+    {
+        Chicken_Anime = gameObject.GetComponent<Animator>();
+        跑步開關 = false;
+    }
+
+
+    void Update()
+
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Chicken_Anime.SetTrigger("吃飯觸發器");
+        }
+        
+        if(Input.GetKey(KeyCode.R))
+        {
+            Chicken_Anime.SetBool("跑步開關", true);
+        }
+
+     
+    }
+}
